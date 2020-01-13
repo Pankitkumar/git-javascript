90ce6ab3e1f7670c051da46964b77df330e61b7e
Pankitkumar <46379029+Pankitkumar@users.noreply.github.com>
2020-01-13 17:21:37 -0500

    Final Commit

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const  instructorName ='geoff'
+let name = 'Pankit'
+console.log(`Hello, my name is ${name},`)
 
+let number = 12
 
-console.log(instructorName)
+console.log(`The number ${number} multiply by 2 is ${number * 2},`)
+console.log(`The number multiplied by 5 is ${product * 5},`)
+console.log(`The result divided by 5 is ${(product * 5) / number}`)
+let finalValue = (product * 5) / number
+console.log(`The result minus 7 is ${finalValue * 7},`)
